26affba3c3121b404ca1f05e3c486d02b89a6888
Matus Gensor <matusgensor123@gmail.com>
2020-09-21 15:55:51 +0200

    implemented player movement, implemented look with mouse

---
+++ Assets/Scripts/LookX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour
+{
+    [SerializeField]
+    private float sensivity = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var lookX = Input.GetAxis("Mouse X");
+
+        Vector3 newEulerAngles = transform.localEulerAngles;
+        newEulerAngles.y += lookX * sensivity;
+        transform.localEulerAngles = newEulerAngles;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Vector3 playerVelocity;
+    private float yVelocity;
+    private bool canDoubleJump;
+    [SerializeField]
+    private float playerSpeed = 5.0f;
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+    [SerializeField]
+    private float gravityValue = 9.81f;
+
+    private CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+    private void HandleMovement()
+    {
+        var horizontalAxis = Input.GetAxis("Horizontal");
+        var verticalAxis = Input.GetAxis("Vertical");
+
+        var direction = new Vector3(horizontalAxis, 0, verticalAxis);
+        playerVelocity = direction * playerSpeed;
+
+        if (characterController.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                yVelocity = jumpHeight;
+            }
+        }
+
+        playerVelocity.y = yVelocity;
+
+        playerVelocity = transform.transform.TransformDirection(playerVelocity);
+
+        characterController.Move(playerVelocity * Time.deltaTime);
+    }
+}
